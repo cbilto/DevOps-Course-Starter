6f5521170e641da166448a4e605b79117531ed97
cbilto <claire.j.bilton@aexp.com>
2020-09-24 11:01:22 +0100

    Step 1

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template, request, redirect, url_for
+import flask
+from flask import Flask, render_template
 import session_items as session
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
+
 @app.route('/')
 def index():
-    return 'Hello World!'
+    # return 'Hello World!'
+    items = session.get_items()
+    session.add_item('complete project excercise 1')
+    return render_template("index.html", items=items)
+
+#@app.route('/', methods=['POST'])
+#def add():
+#    items = session.get_items()
+#    session.add_item(request.form.get('input_title'))
+#    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
